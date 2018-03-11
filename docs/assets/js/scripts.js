d6f421b820d52546c12d3da7a6e1ffe0754adfca
Giagnus64 <28762593+Giagnus64@users.noreply.github.com>
2018-03-11 18:43:03 -0400

    New repo with better dev env.

---
+++ docs/assets/js/scripts.js
@@ -0,0 +1,70 @@
+var button1 = document.querySelector(".bk1");
+var button2 = document.querySelector(".bk2");
+var button3 = document.querySelector(".bk3");
+var button4 = document.querySelector(".bk4");
+var button5 = document.querySelector(".bk5");
+var button6 = document.querySelector(".bk6");
+var button7 = document.querySelector(".bk7");
+var button8 = document.querySelector(".bk8");
+var button9 = document.querySelector(".bk9");
+var button10 = document.querySelector(".bk10");
+var button11 = document.querySelector(".bk11");
+var button12 = document.querySelector(".bk12");
+var button13 = document.querySelector(".bk13");
+
+button1.addEventListener('click', function(){
+	document.body.style.backgroundImage = "url(assets/backgrounds/bk1.jpg)";
+	document.body.style.color = "white";
+});
+button2.addEventListener('click', function(){
+	document.body.style.backgroundImage = "url(assets/backgrounds/bk2.png)";
+	document.body.style.color = "black";
+});
+button3.addEventListener('click', function(){
+	document.body.style.backgroundImage = "url(assets/backgrounds/bk3.png)";
+	document.body.style.color = "white";
+});
+button4.addEventListener('click', function(){
+	document.body.style.backgroundImage = "url(assets/backgrounds/bk4.jpg)";
+	document.body.style.color = "white";
+});
+button5.addEventListener('click', function(){
+	document.body.style.backgroundImage = "url(assets/backgrounds/bk5.jpg)";
+	document.body.style.color = "white";
+});
+button6.addEventListener('click', function(){
+	document.body.style.backgroundImage = "url(assets/backgrounds/bk6.jpg)";
+	document.body.style.color = "white";
+});
+button7.addEventListener('click', function(){
+	document.body.style.backgroundImage = "url(assets/backgrounds/bk7.jpg)";
+	document.body.style.color = "yellow";
+});
+button8.addEventListener('click', function(){
+	document.body.style.backgroundImage = "url(assets/backgrounds/bk8.gif)";
+	document.body.style.color = "white";
+});
+button9.addEventListener('click', function(){
+	document.body.style.backgroundImage = "url(assets/backgrounds/bk9.jpg)";
+	document.body.style.color = "yellow";
+});
+button10.addEventListener('click', function(){
+	document.body.style.backgroundImage = "url(assets/backgrounds/poo.png)";
+	document.body.style.backgroundColor = "#56c1f7"; 
+	document.body.style.color = "black";
+});
+button11.addEventListener('click', function(){
+	document.body.style.backgroundImage = "url(assets/backgrounds/poo.png)";
+	document.body.style.backgroundColor = "#3d3bcc";
+	document.body.style.color = "black";
+});
+button12.addEventListener('click', function(){
+	document.body.style.backgroundImage = "url(assets/backgrounds/stars2.png)";
+	document.body.style.backgroundColor = "#d8ca2d";
+	document.body.style.color = "black";
+});
+button13.addEventListener('click', function(){
+	document.body.style.backgroundImage = "url(assets/backgrounds/stars2.png)";
+	document.body.style.backgroundColor = "#eaadca"
+	document.body.style.color = "black";
+});
